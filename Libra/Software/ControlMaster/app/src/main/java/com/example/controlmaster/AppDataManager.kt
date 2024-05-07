0c372af42cfe9adca32f2210c840fbeafa363089
Rafal <60304472+Bluefish5@users.noreply.github.com>
2024-05-07 15:44:51 +0200

    daily check

---
+++ Libra/Software/ControlMaster/app/src/main/java/com/example/controlmaster/AppDataManager.kt
@@ -0,0 +1,9 @@
+package com.example.controlmaster
+
+class AppDataManager {
+    val listOfDevices = listOf("waga","starter")
+    fun getDevices(): List<String> {
+        return listOfDevices
+    }
+
+}
--- Libra/Software/ControlMaster/app/src/main/java/com/example/controlmaster/MainActivity.kt
+++ Libra/Software/ControlMaster/app/src/main/java/com/example/controlmaster/MainActivity.kt
@@ -1,9 +1,20 @@
 package com.example.controlmaster
 
+import android.content.ClipData
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    val appData = AppDataManager()
+
+    val recyclerView: RecyclerView = findViewById(R.id.myRecyclerView)
+    val adapter = RecyclerViewAdapter(ArrayList())
+
+    recyclerView.adapter = adapter
+    adapter.addItem(appData.getDevices())
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
